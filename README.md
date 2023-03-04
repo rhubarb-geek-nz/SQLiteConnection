f5004bd81e32e80f9c900f5ae926d9d4fc10486e
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2023-03-04 17:01:48 +0000

    First working version

---
+++ README.md
@@ -0,0 +1,5 @@
+# SQLiteConnection
+
+Very simple PowerShell module for creating a connection to an SQLite database.
+
+[Writing Portable Modules](https://learn.microsoft.com/en-us/powershell/scripting/dev-cross-plat/writing-portable-modules?view=powershell-7.3)
---
+++ test.ps1
@@ -0,0 +1,71 @@
+#!/usr/bin/env pwsh
+#
+#  Copyright 2023, Roger Brown
+#
+#  This file is part of rhubarb-geek-nz/SQLiteConnection.
+#
+#  This program is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+
+$ErrorActionPreference = "Stop"
+$ProgressPreference = "SilentlyContinue"
+
+trap
+{
+	throw $PSItem
+}
+
+if (-not(Test-Path "test.db"))
+{
+	@"
+CREATE TABLE MESSAGES (CONTENT VARCHAR(256));
+INSERT INTO MESSAGES (CONTENT) VALUES ('Hello World');
+"@ | & sqlite3 test.db
+
+	If ( $LastExitCode -ne 0 )
+	{
+		Exit $LastExitCode
+	}
+} 
+
+Import-Module "./bin/Release/netstandard2.0/SQLiteConnection.dll"
+
+$Connection = New-SQLiteConnection -ConnectionString "Data Source=test.db;"
+
+try
+{
+	$Connection.Open()
+
+	$Command = $Connection.CreateCommand()
+
+	$Command.CommandText = "SELECT * FROM MESSAGES"
+
+	$Reader = $Command.ExecuteReader()
+
+	try
+	{
+		while ($Reader.Read())
+		{
+			Write-Host $Reader.GetString(0)
+		}
+	}
+	finally
+	{
+		$Reader.Dispose()
+	}
+}
+finally
+{
+	$Connection.Dispose()
+}
